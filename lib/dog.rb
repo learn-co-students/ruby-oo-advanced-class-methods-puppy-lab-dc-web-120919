3fff4c6f00089af560348a71653ae757bd9dfc46
Jennifer Gomez <jennifergomezbruno@gmail.com>
2019-12-15 10:15:50 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,37 +2,31 @@ require 'pry'
 
 class Dog
 
-    attr_reader :name
+    attr_reader :name 
 
     @@all = []
 
     def initialize(name)
         @name = name 
-        save 
+        save
     end
 
-    def self.all #is a class method returns all dog instances
-        @@all
+    def self.all
+        @@all 
     end
 
     def self.clear_all
-        #is a class method that empties the @@all array of all existing dogs
         self.all.clear 
-
     end
 
     def self.print_all
-        #is a class method that puts out the name of each dog to the terminal
         self.all.map do |dog|
-            puts dog.name
+            puts dog.name 
         end
-
     end
 
     def save
-        #adds this dog instance to the @@all array when called
-        @@all << self
-
+        @@all << self 
     end
 
 
